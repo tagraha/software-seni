e6a35db67a2ba1cb470970890f245dd91051eaa3
Tirta Nugraha <dev.nugrata@gmail.com>
2018-03-06 18:29:48 +0700

    style(component): update style for user list component

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 
 class UserList extends Component {
   render() {
+    const { userData } = this.props;
+
     return (
-      <div>list</div>
+        <ul className="list pl0 mt0 measure center">
+          <li className="flex items-center lh-copy pa3 ph0-l bb b--black-10">
+            <img className="w2 h2 w3-ns h3-ns br-100" src={userData.avatar_url} />
+            <div className="pl3 flex-auto">
+              <span className="db black-70">{userData.login}</span>
+              <span className="db black-70">Score:&nbsp;{userData.score}</span>
+            </div>
+            <div>
+              <a target="_blank" href={userData.html_url} className="link blue hover-dark-gray">open in github</a>
+            </div>
+          </li>
+        </ul>
     )
   }
 }
+export default UserList;
