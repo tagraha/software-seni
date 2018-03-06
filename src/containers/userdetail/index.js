2ebf92ab14fa94362af11d661de2569721612e81
Tirta Nugraha <dev.nugrata@gmail.com>
2018-03-06 18:37:31 +0700

    other(route): adding user detail route

---
+++ src/containers/userdetail/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UserDetail extends Component {
+  render() {
+    return (
+      <div>UserDetail</div>
+    )
+  }
+}
+
+export default UserDetail;
