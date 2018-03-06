67a74bfa799030ac2a5a8206abb991376205ee03
Tirta Nugraha <dev.nugrata@gmail.com>
2018-03-06 18:45:49 +0700

    refactor(slug): preparation for getting user detail

--- src/containers/userdetail/index.js
+++ src/containers/userdetail/index.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+import {
+  searchUserReq,
+} from '../../modules/githubusers';
 
 class UserDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug = ""
+    }
+  }
+  componentDidMount() {
+    const { match } = this.props;
+    const slug = match.params.slug
+    this.setState({ slug });
+  }
   render() {
     return (
       <div>UserDetail</div>
@@ -8,4 +25,17 @@ class UserDetail extends Component {
   }
 }
 
-export default UserDetail;
+const mapStateToProps = state => ({
+  githubUsers: state.githubusers.users
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      searchUserReq,
+      changePage: () => push('/about-us')
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserDetail);
